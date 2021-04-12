4e91eea15873bcb6c8c1ee1f49f750e26aea1c80
Dev-Unilord <dev@unilord.io>
2021-04-13 00:45:40 +0900

    Temporaly mainet

--- src/lib/contracts.js
+++ src/lib/contracts.js
@@ -1,8 +1,15 @@
 const contracts = {
   //Mainnet
   1: {
-    ERC: { PEER: "0x1", PEERLP: "0x1", USDT: "0x1" },
-    POOL: { PEER: "0x1", PEERLP: "0x1" }
+    ERC: {
+      PEER: "0x29854184bAa20428219180b2c79986febE556AA1",
+      PEERLP: "0x46e3909253bb715fdc324efeb8c52df663eca22e",
+      USDT: "0x35156Ecb3f7d96575693Ae400Bca6e17Aa934Bf6"
+    },
+    POOL: {
+      PEER: "0x8611629bab57955196B9E0196Cf31F909838A2ED",
+      PEERLP: "0x4A796AF576AD5fD7902E6F0325B0B6BD635835b1"
+    }
   },
   //Ropsten
   3: {
